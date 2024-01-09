3ed4446b8daf94196f7268b5a9770e11193921f9
qoogooo <alice6252002@gmail.com>
2024-01-09 13:54:29 +0800

    新增logincontroller和userrowmapper, 更新userdao

--- src/main/java/com/mycompany/ticketwebsite/DAO/UserDao.java
+++ src/main/java/com/mycompany/ticketwebsite/DAO/UserDao.java
@@ -1,5 +1,6 @@
 package com.mycompany.ticketwebsite.DAO;
 
+import com.mycompany.ticketwebsite.DAO.mapper.UserRowMapper;
 import com.mycompany.ticketwebsite.model.UserRegModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -12,6 +13,11 @@ public class UserDao {
     public int saveUser(UserRegModel user){
         String sql1="INSERT INTO USER(username, password, mobile, `e-mail`) values (?,?,?,?)";
                 return jdbcTemplate.update(sql1,user.getUsername(),user.getPassword(),user.getMobile(),user.getEmail());
+    }
+
+    public UserRegModel findByUsername(String username){
+        String sql3="SELECT * FROM USER where username=?";
+        return jdbcTemplate.queryForObject(sql3, new Object[]{username}, new UserRowMapper());
     }
     public long isUserExists(String username){
         String sql="SELECT COUNT (*) FROM USER WHERE username=?";
