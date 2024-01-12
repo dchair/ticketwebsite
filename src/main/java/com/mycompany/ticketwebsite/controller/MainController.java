1da53613a5ff539c632420de2fcda2fa80b57c13
dchair <a0913809823@gamil.com>
2024-01-12 15:46:24 +0800

    1/12 更新版

--- src/main/java/com/mycompany/ticketwebsite/controller/MainController.java
+++ src/main/java/com/mycompany/ticketwebsite/controller/MainController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
@@ -19,26 +20,59 @@ public class MainController {
         // 返回首页视图
         return "index";
     }
+
     @GetMapping("/concert-page/first")
-    public String concertPageA() {
+    public String concertPageA(Model model, HttpSession session) {
+        // 从会话中获取用户信息
+        UserRegModel user = (UserRegModel) session.getAttribute("user");
+
+        // 将用户信息传递给前端
+        model.addAttribute("user", user);
+
+        // 返回首页视图
         return "concert-page-first";
     }
+
     @GetMapping("/concert-page/second")
-    public String concertPageB() {
+    public String concertPageB(Model model, HttpSession session) {
+        // 从会话中获取用户信息
+        UserRegModel user = (UserRegModel) session.getAttribute("user");
+
+        // 将用户信息传递给前端
+        model.addAttribute("user", user);
+
+        // 返回首页视图
         return "concert-page-second";
     }
+
     @GetMapping("/concert-page/third")
-    public String concertPageC() {
+    public String concertPageC(Model model, HttpSession session) {
+        // 从会话中获取用户信息
+        UserRegModel user = (UserRegModel) session.getAttribute("user");
+
+        // 将用户信息传递给前端
+        model.addAttribute("user", user);
+
+        // 返回首页视图
         return "concert-page-third";
     }
 
+
     @GetMapping("/shopping-cart")
-    public String shoppingCart() {
+    public String shoppingCart(Model model, HttpSession session) {
+        // 从会话中获取用户信息
+        UserRegModel user = (UserRegModel) session.getAttribute("user");
+
+        // 将用户信息传递给前端
+        model.addAttribute("user", user);
+
+        // 返回首页视图
         return "shopping-cart";
     }
-    @GetMapping("/complete-purchase")
-    public String completePurchase() {
-        return "complete-purchase";
+    @GetMapping("/logout")
+    public String logout(HttpSession session){
+        session.removeAttribute("user");
+        return "index";
     }
 
 }
